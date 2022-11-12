cc183697686c44d0b33edda0044f643a5e22bad9
AlinAvram08 <alin.awram@gmail.com>
2022-11-12 16:42:21 +0200

    Add project files.

---
+++ Data/Avram_Alin_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Avram_Alin_Lab2.Models;
+
+namespace Avram_Alin_Lab2.Data
+{
+    public class Avram_Alin_Lab2Context : DbContext
+    {
+        public Avram_Alin_Lab2Context (DbContextOptions<Avram_Alin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Avram_Alin_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Avram_Alin_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Avram_Alin_Lab2.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Avram_Alin_Lab2.Data;
+using Avram_Alin_Lab2.Models;
+
+namespace Avram_Alin_Lab2.Pages.Categories
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Avram_Alin_Lab2.Data.Avram_Alin_Lab2Context _context;
+
+        public DetailsModel(Avram_Alin_Lab2.Data.Avram_Alin_Lab2Context context)
+        {
+            _context = context;
+        }
+
+      public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Category = category;
+            }
+            return Page();
+        }
+    }
+}
