17daad75383e2fc260c90b057b276d8f60122d0a
AlinAvram08 <alin.awram@gmail.com>
2022-11-12 17:38:30 +0200

    Update

---
+++ Models/BookData.cs
@@ -0,0 +1,9 @@
+﻿namespace Avram_Alin_Lab2.Models
+{
+    public class BookData
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+    }
+}
